a4654dbad56d48c99aa8e857b3978a23b646a109
Ceyhun Kayacan <ceyslab@gmail.com>
2022-10-25 08:40:38 +0300

    Edited State Actions

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,17 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { toggle, destroy } from '../redux/todos/todosSlice'
+import { toggle, destroy,selectFilteredTodos } from '../redux/todos/todosSlice'
 
-let filtered = [];
+
 function TodoList() {
     const dispatch = useDispatch();
-    const items = useSelector((state) => state.todos.items)
-    const activeFilter = useSelector((state) => state.todos.activeFilter)
+    const filteredTodos = useSelector(selectFilteredTodos)
 
-    filtered = items;
-    if (activeFilter != 'all') {
-        filtered = items.filter((todo) =>
-            activeFilter === 'active'
-                ? todo.completed === false
-                : todo.completed === true)
-    }
 
     return (
         <ul className="todo-list">
             {
-                filtered.map((item) => (
+                filteredTodos.map((item) => (
                     <li key={item.id} className={item.completed ? 'completed' : ''}>
                         <div className="view">
                             <input className="toggle" type="checkbox" onChange={() => dispatch(toggle({ id: item.id }))} checked={item.completed} />
